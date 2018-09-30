04c1f1898307f3b628dcdf2417ce5faa7d8912ae
tanmancan <contact@tkarimdesign.com>
2018-09-30 12:02:45 -0400

    Better error message handling

--- src/components/RequestResponse.vue
+++ src/components/RequestResponse.vue
@@ -1,16 +1,26 @@
 <template>
-  <p class="response-message" v-if="response.message || response.errors">
-    <span v-if="response.html_url">
-      Your release has been created:
-      <a target="_blank" rel="noopener noreferrer" :href="response.html_url">View Release</a>
+  <div>
+    <p class="response-message" v-if="response.message || response.errors">
+      <span v-if="response.html_url">
+        Your release has been created:
+        <a target="_blank" rel="noopener noreferrer" :href="response.html_url">View Release</a>
+        <br>
+      </span>
+      <span v-if="response.documentation_url">{{response.documentation_url}}</span>
       <br>
-    </span>
-    <span v-if="response.documentation_url">{{response.documentation_url}}<br></span>
-    <span v-if="response.message">{{response.message}}<br></span>
-    <span v-if="response.errors" v-for="(error, idx) in response.errors" :key="idx">
-      <span>{{error.field}} {{error.code}}<br></span>
-    </span>
-  </p>
+      <span v-if="response.message">{{response.message}}</span>
+      <br>
+      <span v-if="response.errors" v-for="(error, idx) in response.errors" :key="idx">
+        <span>Error:{{error.code}}</span>
+        <span v-if="error.field"> - field:{{error.field}}</span>
+        <span v-if="error.message"> - {{error.message}}</span>
+        <br>
+      </span>
+    </p>
+    <p class="error-message" v-if="response.errors">
+
+    </p>
+  </div>
 </template>
 
 <script>
